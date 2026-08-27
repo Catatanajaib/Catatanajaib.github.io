6ab00872cb59562b0787916f8b2706e94612dcb6
Miftah98 <miftahulmujib38@gmail.com>
2026-08-27 21:17:34 +0700

    Update mycssmyjs.js

--- Javascript/mycssmyjs.js
+++ Javascript/mycssmyjs.js
@@ -1,19 +1,26 @@
 class mycssmyjs extends HTMLElement {
   connectedCallback() {
+    // 1. Muat File CSS via innerHTML
     this.innerHTML = `
-<link rel="stylesheet" href="/css/global.css">
-<link rel="stylesheet" href="/css/beranda.css">
-<link rel="stylesheet" href="/css/postingan.css">
-<script src="/Javascript/common.js" defer></script>
-<script src="/Javascript/beranda.js" defer></script>
-<script src="/Javascript/postingan.js" defer></script>
+      <link rel="stylesheet" href="/css/global.css">
+      <link rel="stylesheet" href="/css/beranda.css">
+      <link rel="stylesheet" href="/css/postingan.css">
     `;
+
+    // 2. Muat File JS secara dinamis menggunakan createElement
+    const scripts = [
+      '/Javascript/common.js',
+      '/Javascript/beranda.js',
+      '/Javascript/postingan.js'
+    ];
+
+    scripts.forEach(src => {
+      const script = document.createElement('script');
+      script.src = src;
+      script.defer = true;
+      document.head.appendChild(script);
+    });
   }
 }
 
 customElements.define('mycssmyjs', mycssmyjs);
-
-// untuk memanggil 
-// <mycssmyjs></mycssmyjs>
-
-//  <script src="mycssmyjs.js"></script>
