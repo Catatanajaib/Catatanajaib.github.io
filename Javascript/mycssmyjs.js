faffc3d5b4ee04f84a5a8dc2d4708c611a953a54
Miftah98 <miftahulmujib38@gmail.com>
2026-08-27 21:46:48 +0700

    Update mycssmyjs.js

--- Javascript/mycssmyjs.js
+++ Javascript/mycssmyjs.js
@@ -1,24 +1,24 @@
 class mycssmyjs extends HTMLElement {
   connectedCallback() {
-    // 1. Muat File CSS via innerHTML
+    // 1. Masukkan CSS via innerHTML (CSS tetap jalan via innerHTML)
     this.innerHTML = `
       <link rel="stylesheet" href="/css/global.css">
       <link rel="stylesheet" href="/css/beranda.css">
       <link rel="stylesheet" href="/css/postingan.css">
     `;
 
-    // 2. Muat File JS secara dinamis menggunakan createElement
-    const scripts = [
+    // 2. Buat dan tempelkan tag <script> secara dinamis
+    const jsFiles = [
       '/Javascript/common.js',
       '/Javascript/beranda.js',
       '/Javascript/postingan.js'
     ];
 
-    scripts.forEach(src => {
+    jsFiles.forEach(src => {
       const script = document.createElement('script');
       script.src = src;
       script.defer = true;
-      document.head.appendChild(script);
+      document.head.appendChild(script); // Script disuntikkan ke <head> agar dieksekusi browser
     });
   }
 }
